b75715bcaf5a6d779070fe22913e0863bea2a427
schlanjo <schlanjo@gmail.com>
2021-03-29 17:18:22 -0600

    Build testing for 'getUserData()' method

--- test/userRepository-test.js
+++ test/userRepository-test.js
@@ -8,19 +8,26 @@ describe("UserRepository", () => {
   let data;
 
   before("setup user data", () => {
-    data = {
-      "id": 1, 
-      "name": "Hans Person-Being",
-      "address": "4255 Real St, Actual Town AW 01010-0101",
-      "email": "hans@hotmail.com",
-      "strideLength": 4.2,
-      "dailyStepGoal": 4004,
-      "friends": [
-        2,
-        3,
-        4
-      ]
-    };
+    data = [
+      {
+        "id": 1, 
+        "name": "Hans Person-Being",
+        "address": "4255 Real St, Actual Town AW 01010-0101",
+        "email": "hans@hotmail.com",
+        "strideLength": 4.2,
+        "dailyStepGoal": 4004,
+        "friends": [ 2, 3, 4 ]
+      },
+      {
+        "id": 2,
+        "name": "Tina Human",
+        "address": "2020 Another St, Townes Place AW 10101-1010",
+        "email": "tina@yahoo.com",
+        "strideLength": 4.0,
+        "dailyStepGoal": 9001,
+        "friends": [ 1 , 3, 4 ]
+      }
+    ];
   })
 
   it("should instantiate a User", () => {
@@ -32,4 +39,30 @@ describe("UserRepository", () => {
     const userRepo = new UserRepository(data);
     expect(userRepo.data).to.equal(data);
   })
+
+  describe("getUserData", () => {
+    let userRepo;
+
+    beforeEach("setup initial UserRepository", () => {
+      userRepo = new UserRepository(data);
+    });
+
+    it("should return user-specific data using a user's ID", () => {
+      const firstId = userRepo.data[0].id;
+      const secondId = userRepo.data[1].id;
+      
+      const firstDataSet = userRepo.getUserData(firstId);
+      const secondDataSet = userRepo.getUserData(secondId);
+
+      expect(firstDataSet).to.equal(userRepo.data[0]);
+      expect(secondDataSet).to.equal(userRepo.data[1]);
+    });
+
+    it ("should return 'undefined' if an invalid ID is supplied", () => {
+      const invalidID = 3;
+      const invalidData = userRepo.getUserData(invalidID);
+
+      expect(invalidData).to.be.undefined;
+    })
+  });
 })
