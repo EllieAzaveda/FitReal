352206e4b94439a043bba9eba9727e4a8aa90a46
EllieAzaveda <elliemiller66@icloud.com>
2021-03-29 17:40:55 -0600

    Add Hydration-test file

---
+++ test/HydrationRepository-test.js
@@ -0,0 +1,14 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const HydrationRepository = require('../src/HydrationRepository');
+const data = require('../data/hydration.js');
+
+describe('HydrationRepository', function() {
+
+  it('should be a function', function() {
+  const hydrationRepo = new HydrationRepository();
+  expect(HydrationRepository).to.be.a('function');
+  });
+
+})
